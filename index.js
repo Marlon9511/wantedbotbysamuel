1b452b0f30716cb7183c55c0da938200f5d5e89a
Marlon9511 <marlonkurth79@gmail.com>
2026-04-28 15:00:34 +0200

    Aktualisieren von index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { makeWASocket, useMultiFileAuthState } from "@717Development/baileys";
+import { makeWASocket, useMultiFileAuthState } from "@717development/baileys";
 import pino from "pino";
 import readline from "readline";
 import chalk from "chalk";
