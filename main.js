1c5ccd3f8a5ed11781fb2e9efe41b0996c11419a
Marlon9511 <marlonkurth79@gmail.com>
2026-04-28 14:59:09 +0200

    Aktualisieren von main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log("Script gestartet");
 import sharp from "sharp";
 import path from "path";
 import { exec } from "child_process";
-import { useMultiFileAuthState } from '@angstvorfrauen/baileys';
+import { useMultiFileAuthState } from '@717Development/baileys;
 
 
 
